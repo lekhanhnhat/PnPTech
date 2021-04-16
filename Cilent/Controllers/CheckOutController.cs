7e1744c821374cdefa60b72337f240c28c11b7bc
lekhanhnhat <nhatlk.2000@gmail.com>
2021-04-16 13:44:16 +0700

    PnPProject

---
+++ Cilent/Controllers/CheckOutController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cilent.Controllers
+{
+    public class CheckOutController : Controller
+    {
+        // GET: CheckOut
+        public ActionResult AddressAndPayment()
+        {
+            return View();
+        }
+        public ActionResult Complete()
+        {
+            return View();
+        }
+    }
+}
